c017d3d0ca80e4c613efef7daefcd96c5ebca09f
Martin Clifford <mclifford82@gmail.com>
2017-04-26 15:54:02 -0700

    Update StackingPrep.py
    Added segmentation into groups via itertools pairwise recipe
    Next up is moving files into created directories
    Next Next up is generating a Zerene Stacker batch manifest

--- StackingPrep.py
+++ StackingPrep.py
@@ -1,13 +1,37 @@
 from os import listdir
 from os.path import isfile, join, getmtime
+from itertools import tee
+
+# from itertools
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+filegroup = 1
 
 fullpath = 'C:\\py\\StackingPrep\\'
 
 files = [
-	[f, getmtime(join(fullpath, f))]
+	[f, getmtime(join(fullpath, f)), 0]
 	for f in listdir(fullpath) 
-	if isfile(join(fullpath, f))
+	if isfile(join(fullpath, f)) and f.endswith('.txt') #change later
 ]
 
-for k, v in enumerate(files):
-	print('File {}: {} modified at {}'.format(k, v[0], v[1]))
+# Sort by modified timestamp
+files.sort(key=lambda x: x[1])
+
+# Group files together
+for file, nextfile in pairwise(files):
+	# if the next file's mtime is within 5 seconds, assign group. Otherwise, increment and assign group
+	if nextfile[1] - file[1] < 6:
+		file[2] = filegroup
+		nextfile[2] = filegroup
+	else:
+		file[2] = filegroup
+		filegroup += 1
+		nextfile[2] = filegroup
+
+for f in files:
+	print(f)
